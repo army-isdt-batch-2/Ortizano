68b20ee94e9a04c602be68e1ee46b7a9283fbb62
Ortizano <lordortizano08@gmail.com>
2021-05-25 16:45:32 -0700

    seeder

--- database/migrations/2021_05_24_214209_create_user_table.php
+++ database/migrations/2021_05_24_214209_create_user_table.php
@@ -15,6 +15,11 @@ class CreateUserTable extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('username');
+            $table->string('password');
+
+
             $table->timestamps();
         });
     }
